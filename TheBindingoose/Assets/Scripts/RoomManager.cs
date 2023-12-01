920c40936efda6cd2a874776c004871880ab0a20
Oriol <oriol.gali.7e6@itb.cat>
2023-12-01 17:47:41 +0100

    RoomInstantiation
    +Se instancian i colocan las habitaciones
    +Hay que arreglar el agolpamiento
    +falta la logica para que se conecten

--- TheBindingoose/Assets/Scripts/RoomManager.cs
+++ TheBindingoose/Assets/Scripts/RoomManager.cs
@@ -7,7 +7,7 @@ public class RoomManager : MonoBehaviour
     [SerializeField] private int maxRooms = 15;
     [SerializeField] private int minRooms = 7;
 
-    int roomWidth = 12;
+    int roomWidth = 13;
     int roomHeight = 10;
 
     int gridSizeX = 10;
@@ -21,6 +21,8 @@ public class RoomManager : MonoBehaviour
 
     private int roomCount;
 
+    private bool generationComplete;
+
     private void Start()
     {
         roomGrid = new int[gridSizeX, gridSizeY];
@@ -28,6 +30,29 @@ public class RoomManager : MonoBehaviour
 
         Vector2Int initialRoomIndex = new Vector2Int(gridSizeX / 2,gridSizeY / 2 );
         StartRoomGeneration(initialRoomIndex);
+    }
+
+    private void Update()
+    {
+        if (roomQueue.Count > 0 && roomCount < maxRooms && !generationComplete)
+        {
+            Vector2Int roomIndex = roomQueue.Dequeue();
+            int gridX = roomIndex.x;
+            int gridY = roomIndex.y;
+
+            //Left
+            TryGenerateRoom(new Vector2Int(gridX - 1, gridY));
+            //Right
+            TryGenerateRoom(new Vector2Int(gridX + 1, gridY));
+            //Up
+            TryGenerateRoom(new Vector2Int(gridX, gridY + 1));
+            //Down
+            TryGenerateRoom(new Vector2Int(gridX, gridY - 1));
+        } else if (!generationComplete)
+        {
+            Debug.Log($"Generation complete, {roomCount} rooms");
+            generationComplete = true;
+        }
     }
 
     private void StartRoomGeneration(Vector2Int roomIndex)
@@ -41,6 +66,23 @@ public class RoomManager : MonoBehaviour
         initialRoom.name = $"Room-{roomCount}";
         initialRoom.GetComponent<Room>().RoomIndex = roomIndex;
         roomObjects.Add(initialRoom);
+    }
+
+    private bool TryGenerateRoom(Vector2Int roomIndex)
+    {
+        int x = roomIndex.x;
+        int y = roomIndex.y;
+
+        roomQueue.Enqueue(roomIndex);
+        roomGrid[x, y] = 1;
+        roomCount++;
+
+        var initialRoom = Instantiate(roomPrefab, GetPositionFromGridIndex(roomIndex), Quaternion.identity);
+        initialRoom.name = $"Room-{roomCount}";
+        initialRoom.GetComponent<Room>().RoomIndex = roomIndex;
+        roomObjects.Add(initialRoom);
+
+        return true;
     }
 
     private Vector3 GetPositionFromGridIndex(Vector2Int gridIndex)
