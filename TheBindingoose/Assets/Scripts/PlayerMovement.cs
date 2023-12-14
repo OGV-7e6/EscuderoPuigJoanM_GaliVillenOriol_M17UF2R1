fb606c77a01875c85fe9e2286e72b1bcc17f03ae
Escudero22 <joan.escudero.7e6@itb.cat>
2023-12-14 17:19:34 +0100

    Nosequecommites

--- TheBindingoose/Assets/Scripts/PlayerMovement.cs
+++ TheBindingoose/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,29 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _movementInput;
     [SerializeField] private float _speed;
 
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+
+        if (hor != 0 || ver != 0)
+        {
+            animator.SetFloat("Horizontal", hor);
+            animator.SetFloat("Vertical", ver);
+            animator.SetFloat("Velocidad", 1);
+        }
+        else
+        {
+            animator.SetFloat("Velocidad", 0);
+
+        }
+    }
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
