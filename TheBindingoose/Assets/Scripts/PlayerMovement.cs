08cc79e9890819a7ba21f39e33dd7cee6dcd88d6
Escudero22 <joan.escudero.7e6@itb.cat>
2023-12-27 12:06:36 +0100

    DaveIsRotating

--- TheBindingoose/Assets/Scripts/PlayerMovement.cs
+++ TheBindingoose/Assets/Scripts/PlayerMovement.cs
@@ -6,44 +6,35 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _rb;
     private Vector2 _movementInput;
     [SerializeField] private float _speed;
-    //[SerializeField] private float rotationSpeed = 10f; // Ajusta el valor según sea necesario
     private PlayerInput _playerInput;
-    //private Transform objetoVacio;
+    private Transform objetoVacio;
     private Animator animator;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _playerInput = GetComponent<PlayerInput>();
+        objetoVacio = transform.Find("Move it Dave"); // Asegúrate de que el objeto vacío está anidado en el jugador
     }
 
     private void OnEnable()
     {
         _playerInput.actions["Move"].performed += ctx => Move(ctx);
         _playerInput.actions["Move"].canceled += ctx => Move(ctx);
-        //_playerInput.actions["Look"].performed += ctx => Look(ctx);
-        //_playerInput.actions["Look"].canceled += ctx => Look(ctx);
+        _playerInput.actions["Look"].performed += ctx => Look(ctx);
+        _playerInput.actions["Look"].canceled += ctx => Look(ctx);
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        //objetoVacio = GameObject.Find("Move It Dave").transform;
     }
-
-    //public void Look(InputAction.CallbackContext ctx)
-    //{
-    //    float rotationAmount = ctx.ReadValue<Vector2>().x * rotationSpeed * Time.deltaTime;
-    //    objetoVacio.transform.Rotate(Vector3.up, rotationAmount);
-    //}
 
     public void Move(InputAction.CallbackContext ctx)
     {
-        //float hor = Input.GetAxisRaw("Horizontal");
-        //float ver = Input.GetAxisRaw("Vertical");
         float hor = ctx.ReadValue<Vector2>().x;
         float ver = ctx.ReadValue<Vector2>().y;
+
         if (hor != 0 || ver != 0)
         {
             animator.SetFloat("Horizontal", hor);
@@ -53,19 +44,39 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             animator.SetFloat("Velocidad", 0);
+        }
 
+        // Actualiza la dirección de movimiento para el movimiento del jugador
+        _movementInput = new Vector2(hor, ver);
+    }
+
+    public void Look(InputAction.CallbackContext ctx)
+    {
+        if (ctx.control.device is Keyboard || ctx.control.device is Gamepad)
+        {
+            Vector2 input = ctx.ReadValue<Vector2>();
+
+            if (input != Vector2.zero)
+            {
+                // Calcula el ángulo de rotación directamente
+                float targetAngle = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg;
+
+                // Ajusta el ángulo a 0, 90, 180 o 270 grados
+                targetAngle = Mathf.Round(targetAngle / 90) * -90;
+
+                // Aplica la rotación al objeto vacío
+                objetoVacio.rotation = Quaternion.Euler(0, 0, targetAngle);
+            }
         }
     }
+
 
 
     private void FixedUpdate()
     {
         _rb.velocity = _movementInput * _speed;
     }
-
-    private void OnMove(InputValue input)
-    {
-        _movementInput = input.Get<Vector2>();
-    }
-
 }
+
+
+
